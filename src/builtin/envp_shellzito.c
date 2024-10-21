e892ae8f2de5abe2af8a747571800ebd3f37c2d1
jbolanho <juliabolanho@gmail.com>
2024-10-21 14:12:59 -0300

    ENV copy working

--- src/builtin/envp_shellzito.c
+++ src/builtin/envp_shellzito.c
@@ -1,3 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   envp_shellzito.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbolanho <jbolanho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 13:48:56 by jbolanho          #+#    #+#             */
+/*   Updated: 2024/10/21 13:48:57 by jbolanho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include "../include/minishell.h"
 
 //na main tem que ter envp como variavel
@@ -5,26 +18,28 @@
 void	copy_env()
 {
 	char	**our_env;
+	char	**env;
 	int		i;
 
 	i = 0;
-	while (__environ[i])
+	env = __environ;
+	while (env[i])
 		i++;
 	our_env = malloc (sizeof(char*) * (i + 1));
 	i = 0;
-	while (__environ[i])
+	while (env[i])
 	{
-		our_env[i] = ft_strdup(__environ[i]);
+		our_env[i] = ft_strdup(env[i]);
 		if (!our_env[i])
 		{
-			free_env(our_env);
-			exit(EXIT_FAILURE);
+			while (i--)
+				free(our_env[i]);
+			free(our_env);
 		}
 		i++;
 	}
-	our_env[i] = NULL;
-	print_env_copy(our_env);
-	// env_shellzito(our_env);
+	// print_env_copy(our_env);
+	env_shellzito(our_env);
 }
 
 void	free_env(char **array)
@@ -32,26 +47,22 @@ void	free_env(char **array)
 	int	i;
 
 	i = 0;
-	while(array[i])
+	while (array[i])
 	{
 		free(array[i]);
 		i++;
 	}
+	free(array);
 }
 
-// char	**env_shellzito(char **our_env)
-// {
-// 	static char	**env_shellzito;
+char	**env_shellzito(char **our_env)
+{
+	static char	**env_shellzito;
 
-// 	// if (our_env && env_shellzito)
-// 	// 	ft_free_list(&env_shellzito);
-// 	if (our_env)
-// 		env_shellzito = our_env;
-// 	print_env_copy(env_shellzito);
-// 	return (env_shellzito);
-// }
-
-
-
-
-// usar getenv
+	if (our_env && env_shellzito)
+		free_env(env_shellzito);
+	if (our_env)
+		env_shellzito = our_env;
+	// print_env_copy(env_shellzito);
+	return (env_shellzito);
+}
